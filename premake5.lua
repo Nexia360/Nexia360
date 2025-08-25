11a457f426a83fbd087a8203b517470365e450be
emoose <abc@cock.li>
2025-08-25 16:26:23 +0100

    [GDBStub] Make stub Windows-only
    Only Windows has a socket impl, so made GDBStub Windows-only right now.

--- premake5.lua
+++ premake5.lua
@@ -339,7 +339,6 @@ workspace("xenia")
   include("src/xenia/base")
   include("src/xenia/cpu")
   include("src/xenia/cpu/backend/x64")
-  include("src/xenia/debug/gdb")
   include("src/xenia/debug/ui")
   include("src/xenia/gpu")
   include("src/xenia/gpu/null")
@@ -361,6 +360,7 @@ workspace("xenia")
 
   if os.istarget("windows") then
     include("src/xenia/apu/xaudio2")
+    include("src/xenia/debug/gdb") -- Needs socket support for other systems.
     include("src/xenia/gpu/d3d12")
     include("src/xenia/hid/winkey")
     include("src/xenia/hid/xinput")
