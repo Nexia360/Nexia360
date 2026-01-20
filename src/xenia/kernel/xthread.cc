a8719d57dce27fa3b3a7f1cf8149c49aed868de1
philtimmes <philtimmes@gmail.com>
2026-01-20 08:52:25 -0500

    Fixing Xenia Memory leaks

--- src/xenia/kernel/xthread.cc
+++ src/xenia/kernel/xthread.cc
@@ -878,11 +878,13 @@ object_ref<XThread> XThread::Restore(KernelState* kernel_state,
   thread->kernel_state_ = kernel_state;
 
   if (!thread->RestoreObject(stream)) {
+    delete thread;
     return nullptr;
   }
 
   if (stream->Read<uint32_t>() != kThreadSaveSignature) {
     XELOGE("Could not restore XThread - invalid magic!");
+    delete thread;
     return nullptr;
   }
 
